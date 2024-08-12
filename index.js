e9a5eee23cfddf3e9cd59494ac649c973777339b
Barros99 <jeanmeneses@hotmail.com>
2024-08-12 01:07:40 -0300

    feat: integrate main function from app module and execute
    - Imported the `main` function from the `app` module.
    - Added call to `main` function with database instance in the execution flow.

--- index.js
+++ index.js
@@ -1,86 +1,81 @@
 const { DATABASE_SCHEMA, DATABASE_URL, SHOW_PG_MONITOR } = require('./config');
 const massive = require('massive');
 const monitor = require('pg-monitor');
+const { main } = require('./app');
 
 // Call start
 (async () => {
-    console.log('main.js: before start');
+  console.log('main.js: before start');
 
-    const db = await massive({
-        connectionString: DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-    }, {
-        // Massive Configuration
-        scripts: process.cwd() + '/migration',
-        allowedSchemas: [DATABASE_SCHEMA],
-        whitelist: [`${DATABASE_SCHEMA}.%`],
-        excludeFunctions: true,
-    }, {
-        // Driver Configuration
-        noWarnings: true,
-        error: function (err, client) {
-            console.log(err);
-            //process.emit('uncaughtException', err);
-            //throw err;
+  const db = await massive(
+    {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    {
+      // Massive Configuration
+      scripts: process.cwd() + '/migration',
+      allowedSchemas: [DATABASE_SCHEMA],
+      whitelist: [`${DATABASE_SCHEMA}.%`],
+      excludeFunctions: true,
+    },
+    {
+      // Driver Configuration
+      noWarnings: true,
+      error: function (err, client) {
+        console.log(err);
+        //process.emit('uncaughtException', err);
+        //throw err;
+      },
+    },
+  );
+
+  if (!monitor.isAttached() && SHOW_PG_MONITOR === 'true') {
+    monitor.attach(db.driverConfig);
+  }
+
+  const execFileSql = async (schema, type) => {
+    return new Promise(async (resolve) => {
+      const objects = db['user'][type];
+
+      if (objects) {
+        for (const [key, func] of Object.entries(objects)) {
+          console.log(`executing ${schema} ${type} ${key}...`);
+          await func({
+            schema: DATABASE_SCHEMA,
+          });
         }
+      }
+
+      resolve();
     });
+  };
 
-    if (!monitor.isAttached() && SHOW_PG_MONITOR === 'true') {
-        monitor.attach(db.driverConfig);
-    }
+  //public
+  const migrationUp = async () => {
+    return new Promise(async (resolve) => {
+      await execFileSql(DATABASE_SCHEMA, 'schema');
 
-    const execFileSql = async (schema, type) => {
-        return new Promise(async resolve => {
-            const objects = db['user'][type];
+      //cria as estruturas necessarias no db (schema)
+      await execFileSql(DATABASE_SCHEMA, 'table');
+      await execFileSql(DATABASE_SCHEMA, 'view');
 
-            if (objects) {
-                for (const [key, func] of Object.entries(objects)) {
-                    console.log(`executing ${schema} ${type} ${key}...`);
-                    await func({
-                        schema: DATABASE_SCHEMA,
-                    });
-                }
-            }
+      console.log(`reload schemas ...`);
+      await db.reload();
 
-            resolve();
-        });
-    };
+      resolve();
+    });
+  };
 
-    //public
-    const migrationUp = async () => {
-        return new Promise(async resolve => {
-            await execFileSql(DATABASE_SCHEMA, 'schema');
+  try {
+    await migrationUp();
 
-            //cria as estruturas necessarias no db (schema)
-            await execFileSql(DATABASE_SCHEMA, 'table');
-            await execFileSql(DATABASE_SCHEMA, 'view');
+    await main(db);
 
-            console.log(`reload schemas ...`)
-            await db.reload();
-
-            resolve();
-        });
-    };
-
-    try {
-        await migrationUp();
-
-        //exemplo de insert
-        const result1 = await db[DATABASE_SCHEMA].api_data.insert({
-            doc_record: { 'a': 'b' },
-        })
-        console.log('result1 >>>', result1);
-
-        //exemplo select
-        const result2 = await db[DATABASE_SCHEMA].api_data.find({
-            is_active: true
-        });
-        console.log('result2 >>>', result2);
-
-    } catch (e) {
-        console.log(e.message)
-    } finally {
-        console.log('finally');
-    }
-    console.log('main.js: after start');
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    console.log('finally');
+  }
+  console.log('main.js: after start');
 })();
